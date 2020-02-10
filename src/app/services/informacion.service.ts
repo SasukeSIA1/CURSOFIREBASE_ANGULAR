95c968a89ea5eff59ab1cd24313d6b9e546eb40d
José Gallardo <gallardoojose@gmail.com>
2020-02-10 10:27:44 -0700

    Services Firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -9,15 +9,33 @@ export class InformacionService {
   info: any = {};
   cargada:boolean = false;
 
-  constructor(public http: HttpClient) {
+  equipo: any = [];
+  cargada_sobre_nosotros: boolean = false;
 
+  constructor(public http: HttpClient) {
+    this.carga_info();
+    this.cargar_sobre_nosotros();
+  }
+
+  public carga_info(){
     this.http.get("assets/data/info.pagina.json")
       .subscribe(data => {
 
-        console.log(data);
+        /**console.log(data);*/
         this.info = data;
         this.cargada = true;
-      });
+      });      
+  }
 
-   }
+  public cargar_sobre_nosotros(){
+    this.http.get('https://civic-badge-254923.firebaseio.com/equipo.json')
+      .subscribe(data =>{
+        
+        console.log(data)
+        this.equipo = data[0];
+        this.cargada_sobre_nosotros = true;
+      });
+  }
+
+   
 }
